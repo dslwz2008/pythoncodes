c060ad4c7ddb2d2d14681f83061f9aa5cff584da
Shen Shen <shenshen@Shens-MacBook-Pro.local>
2013-09-26 09:21:04 +0800

    binary

--- algorithms/binary_search_tree.py
+++ algorithms/binary_search_tree.py
@@ -1,2 +1,16 @@
 #-*-coding:utf-8-*-
 __author__ = 'shenshen'
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
