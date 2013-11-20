33902d83afd05548c14a19d3ba2b3bfcd9263d76
dslwz2008@gmail.com <dslwz020415>
2013-11-20 17:26:46 +0800

    modify 02 file

--- dsalgo001/ex0202.py
+++ dsalgo001/ex0202.py
@@ -1,10 +1,14 @@
 #-*-coding:utf-8-*-
 __author__ = 'shenshen'
 
-
+from stack import Stack
 
 def main():
-    pass
+    m = int(raw_input())
+    for i in range(m):
+        n = int(raw_input())
+        for j in range(n):
+            pass
 
 if __name__ == '__main__':
     main()
