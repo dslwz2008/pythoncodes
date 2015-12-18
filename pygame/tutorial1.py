a8809d033e79c56ebf7336bcfa383bbda241d14b
shenshen <dslwz2008@gmail.com>
2015-12-18 22:16:44 +0800

    add pygame tutorials

---
+++ pygame/tutorial1.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8-*-
+# Author: Shen Shen
+# Email: dslwz2002@163.com
+
+import pygame
+
+background_colour = (255, 255, 255)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Tutorial 1')
+screen.fill(background_colour)
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
